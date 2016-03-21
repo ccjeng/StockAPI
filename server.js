110bbdfee724f65c79e49a3d4546a1584bd82a72
Andy Cheng <ccjeng@gmail.com>
2016-03-21 12:06:36 +0000

    init

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var request = require('request');
+
+var server = express();
+
+server.get('/q/:symbols', function (req, res) {
+   
+	var quoteUrl = 'http://www.google.com/finance/info?infotype=infoquoteall&q=' + req.params.symbols;
+   
+   	request(quoteUrl, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        res.end( body.replace("//","") );
+	     } else {
+	     	res.end( 'error' );
+	     }
+	});
+
+
+})
+
+/*
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});*/
+
+// Launch server
+
+var port = process.env.PORT || 8080;
+//    , ip = process.env.IP || "127.0.0.1";
+
+server.listen(port, function() {
+  console.log("Listening on " + port);
+});
