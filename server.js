0e25287e398ab72302a65fccd4dac0bf628c483a
Andy Cheng <ccjeng@gmail.com>
2016-03-22 03:08:20 +0000

    change config

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/q/:symbols', function (req, res) {
 })
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 app.listen(server_port, server_ip_address, function () {
