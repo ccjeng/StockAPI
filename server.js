dee61693d4070da79340893f21a2b0b4c99dcb54
Andy Cheng <ccjeng@gmail.com>
2016-03-22 02:09:29 +0000

    add openshift server config

--- server.js
+++ server.js
@@ -22,20 +22,20 @@ app.get('/q/:symbols', function (req, res) {
 })
 
 
-/*
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-server.listen(server_port, server_ip_address, function () {
+app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + port )
-});*/
+});
 
 // Launch server
-
+/*
 var port = process.env.PORT || 8080;
 //    , ip = process.env.IP || "127.0.0.1";
 
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
